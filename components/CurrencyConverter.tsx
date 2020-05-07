818e20e678a1d9bf2022b3c765b4a29252903c53
Jakub Žitný <jakubzitny@avocode.com>
2020-05-07 11:31:42 +0200

    init next app

---
+++ components/CurrencyConverter.tsx
@@ -0,0 +1,9 @@
+import Head from 'next/head'
+
+export default function CurrencyConverter() {
+  return (
+    <h1>
+      Currency Converter
+    </h1>
+  )
+}
---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+import CurrencyConverter from '../components/CurrencyConverter'
+
+
+export default function index() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Currency Converter</title>
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+
+      <main>
+        <CurrencyConverter />
+      </main>
+    </div>
+  )
+}
